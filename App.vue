73c857f00190895f133a1f755d3c5b5873d6c1b6
xiaoshuai <991893761@qq.com>
2020-08-21 09:12:57 +0800

    frist

--- App.vue
+++ App.vue
@@ -1,7 +1,20 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-08-21 08:36:54
+ * @LastEditTime: 2020-08-21 09:04:04
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \gulishop_fengshuai\src\App.vue
+-->
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>冯帅</h2>
+    <h2>冯帅</h2>
+    <h2>冯帅</h2>
+    <h2>冯帅</h2>
+    <h2>冯帅</h2>
   </div>
 </template>
 
